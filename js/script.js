7c2809db41bf04cef974001b725026c5d6e07d00
Sean-Thomo <seansthomo@gmail.com>
2022-08-17 17:38:23 +0200

    Bare game setup

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Gameboard module
+let gameboardMod = { 
+  function() {
+    let gameboard = [];
+    return
+  }
+}
+
+// Display controller module
+let displayControllerMod = (function() {
+  let testOne = () => {
+    console.log("Testing private function call inside module object")
+  };
+  return {testOne}
+})();
+
+// Player Factory function
+let createPlayer = (playerName, playerNum, assignedXO) => {
+  let getPlayerName = () => {playerName;
+  console.log(`Player Name: ${playerNum}... ${playerName}`); }
+  return {getPlayerName, playerName, playerNum, assignedXO};
+}
+
+// TODO: Create inputs for name and preffered sign (X or O)
+
+let playerOne = ("Sean", 1, "X");
+let playertwo = ("Other", 2, "O")
